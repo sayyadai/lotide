f0ae95b59d8eb9ad3f682879ce8e9078dcaf9bd6
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-10-25 21:50:12 -0400

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  const succeed = `👍👍👍 Assertion Passed: ${actual} === ${expected}`;
+  const fail = `💩💩💩 Assertion Failed: ${actual} === ${expected}`;
+  if (actual === expected) {
+    console.log(succeed);
+  } else {
+    console.log(fail);
+  }
+
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 4);
+assertEqual("Basketball", "Basketball");
+assertEqual(1, 1);
