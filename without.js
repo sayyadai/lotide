94f5103cb981881f8943beb594f1a21dacae27d0
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-10-30 00:43:33 -0400

    Addding additional test cases

--- without.js
+++ without.js
@@ -28,3 +28,7 @@ const without = function(actual, itemsToRemove) {
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"] ); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
