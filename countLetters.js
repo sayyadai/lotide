9efe91461505fb7e41359800a2ede3f83e9ecfe7
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-11-01 17:36:10 -0400

    countLetters assignment

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  const succeed = `👍👍👍 Assertion Passed: ${actual} === ${expected}`;
+  const fail = `💩💩💩 Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(succeed);
+  } else {
+    console.log(fail);
+  }
+};
+
+const countLetters = function (sentence) {
+  let output = {};
+  
+  for (let letter of sentence) {
+    output[letter] = (output[letter] || 0)  + 1;
+  }
+  return output
+ 
+}
+const result1 = countLetters("LHL")
+
+assertEqual(result1["L"], 2);
+assertEqual(result1["H"], 1);
+
