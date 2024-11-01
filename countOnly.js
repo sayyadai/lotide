7d04013b390ed3bf177ece88aa9a28e4dfa64915
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-11-01 15:34:21 -0400

    linted version

--- countOnly.js
+++ countOnly.js
@@ -10,20 +10,20 @@ const assertEqual = function(actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
-const results = {};
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
-for (const item of allItems) {
-  if (itemsToCount[item]) {
-  if (results[item]) {
-    results[item] += 1;
-  } else {
-    results[item] = 1;
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
   }
-}
-}
 
-    return results
+  return results;
 };
 
 
