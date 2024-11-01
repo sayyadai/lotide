fde05969cb1ee42fba0c646a16e36e718bcf78ba
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-11-01 17:42:04 -0400

    linted version

--- countLetters.js
+++ countLetters.js
@@ -8,16 +8,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function (sentence) {
+const countLetters = function(sentence) {
   let output = {};
   
   for (let letter of sentence) {
     output[letter] = (output[letter] || 0)  + 1;
   }
-  return output
- 
-}
-const result1 = countLetters("LHL")
+  return output;
+};
+const result1 = countLetters("LHL");
 
 assertEqual(result1["L"], 2);
 assertEqual(result1["H"], 1);
