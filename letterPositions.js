5b03c284e37c1aa9d852ddc3c683f3753cbd9db2
Sayyad Yeganehnia <sayyad_yeganehnia@hotmail.com>
2024-11-02 06:35:04 -0400

    letterposition assignment

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(actual, expected) {
+  const succeed = `👍👍👍 Assertion Passed: ${actual} === ${expected}`;
+  const fail = `💩💩💩 Assertion Failed: ${actual} !== ${expected}`;
+  if (eqArrays(actual, expected)) {
+    console.log(succeed);
+  } else {
+    console.log(fail);
+  }
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return (false);
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i])
+      return false;
+  }
+  
+  return true;
+};
+
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+ 
+};
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
